4c683510013a2b8ffff82027b1e0fdac4cb3332e
macek <macek@dal.ca>
2023-04-18 20:53:38 -0300

    axis: fix whitespace

--- src/axis.tsx
+++ src/axis.tsx
@@ -15,17 +15,15 @@ function computeUnit(t: tnullable<taxis>, q: number) {
   if (t == null) return q
   const { intervals: s } = t
   if (s.length < 2) return -Infinity
-    let ax, ay, bx, by
-    let i = 0
-    while (i < s.length - 1) {
-      [ax, ay] = s[i];
-      [bx, by] = s[i + 1]
-      if (q <= bx) {
-        return ay + (by - ay) * (q - ax) / (bx - ax)
-      }
-      i += 1
-    }
-    return -Infinity
+  let ax, ay, bx, by
+  let i = 0
+  while (i < s.length - 1) {
+    [ax, ay] = s[i];
+    [bx, by] = s[i + 1]
+    if (q <= bx) return ay + (by - ay) * (q - ax) / (bx - ax)
+    i += 1
+  }
+  return -Infinity
 }
 
 function computeRect(tx: taxis, ty: taxis, rect: trect) {
